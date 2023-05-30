ac4ad819b2c9377808a1ac3ac64ff9ea85ea0f03
Brayan-Baquero-Rojas <119459369+Brayan-Baquero-Rojas@users.noreply.github.com>
2023-05-30 02:19:24 -0500

    Update listaDeContactos.js
    Se realiza el  código para la creación del objeto para la lista de contactos y la función de eliminar el contacto.

--- listaDeContactos.js
+++ listaDeContactos.js
@@ -1,33 +1,33 @@
-let listaDeContactos = ['Tatiana Buitrago', 'Camilo Velez', 'Arnol Rodriguez', 'Fredy Vega' ];
+let listaDeContactos = [];
 
-// función para añadir un nuevo contacto a una lista
-function agregarContacto(nombreDelContacto){
-    listaDeContactos.push(nombreDelContacto); 
+
+function agregarContacto(id, nombres, apellidos, telefono, ubicaciones){
+      let contacto = {
+    id: id,
+    nombres: nombres,
+    apellidos: apellidos,
+    telefono: telefono,
+    ubicaciones: ubicaciones,
+  };
+  listaDeContactos.push(contacto);
 }
 
-//función para borrar un contacto existente de la lista
 
-function borrarContacto(contactoABorrar){
-    let indiceContacto = listaDeContactos.indexOf(contactoABorrar);
-    listaDeContactos.splice(indiceContacto, 1);
-}
 
-// función para imprimir en consola los contactos presentes en la lista
-function imprimirContactos(){
-    console.log(`Lista de Contactos`)
-    for(let i = 0; i < (listaDeContactos.length); i++){
-        console.log(listaDeContactos[i]);
+
+function borrarContacto(idContactoABorrar){
+     let indiceContacto = -1;
+    for (let i = 0; i < listaDeContactos.length; i++) {
+        if (listaDeContactos[i].id === idContactoABorrar) {
+        indiceContacto = i;
+        break;
+        }
+    }
+
+    if (indiceContacto !== -1) {
+        listaDeContactos.splice(indiceContacto, 1);
     }
 }
 
-// Prueba de la funcion para añadir un nuevo contacto a una lista
-agregarContacto('Camilo Prada');
 
-
-//prueba de la Función para imprimir en consola los contactos presentes en la lista
-    imprimirContactos()
-
-
-
-//Prueba DE la función para borrar un contacto existente de la lista
-//borrarContacto('Tatiana Buitrago');
+    
